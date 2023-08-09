d0fd39b9b578224b3fb5fec8096df6c708afd445
Mir Hamza <122245138+hamzaowaisog@users.noreply.github.com>
2023-08-10 03:02:45 +0500

    Update Lecture3.py

--- Lecture3.py
+++ Lecture3.py
@@ -11,15 +11,18 @@ print(arr.shape)
 
 arr1 = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12])
 newarr = arr1.reshape(4, 3)
-print("Converting 1-D to 2-D:")
+print("\nConverting 1-D to 2-D:")
 print(newarr)
+print(newarr.shape)
 
 # converting 1-D to 3-D
 newarr1 = arr1.reshape(2, 3, 2)
-print("Converting 1-D to 3-D:")
+print("\nConverting 1-D to 3-D:")
 print(newarr1)
+print(newarr1.shape)
 
 # flattening of an array i.e. converting 2-D or 3-D to 1-D
 newarr2 = arr.reshape(-1)
-print("Flattening of an array:")
+print("\nFlattening of an array:")
 print(newarr2)
+print(newarr2.shape)
