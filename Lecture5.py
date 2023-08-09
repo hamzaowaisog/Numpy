0b047883178067be3af80cf7d9919646fd436914
Mir Hamza <122245138+hamzaowaisog@users.noreply.github.com>
2023-08-10 03:28:16 +0500

    Iterating over 3-D arrays

--- Lecture5.py
+++ Lecture5.py
@@ -21,3 +21,23 @@ print("\nIterating each scalar element of 2-D array:")
 for x in arr1:
     for y in x:
         print(y)
+
+# 3-D array
+# Iterating each dimension of 3-D array
+print("\n3-D array:")
+arr2 = np.array([[[1, 2, 3], [4, 5, 6]], [[7, 8, 9], [10, 11, 12]]])
+print("Iterating each dimension of 3-D array:")
+for x in arr2:
+    print(x)
+
+# Iterating each scalar element of 3-D array
+print("\nIterating each scalar element of 3-D array:")
+for x in arr2:
+    for y in x:
+        for z in y:
+            print(z)
+
+# Iterating each scalar element of 3-D array using nditer()
+print("\nIterating each scalar element of 3-D array using nditer():")
+for x in np.nditer(arr2):
+    print(x)
