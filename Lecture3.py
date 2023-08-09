190dc8eb16175e075120e4868daa5fd213c47bc0
Mir Hamza <122245138+hamzaowaisog@users.noreply.github.com>
2023-08-10 02:58:30 +0500

    Shaping and reshaping of an array

---
+++ Lecture3.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+# shaping of an array
+
+arr = np.array([[1, 2, 3, 4], [5, 6, 7, 8]])
+print("Shape of an array:")
+print(arr.shape)
+
+# Reshaping of an array
+# 1-D to 2-D
+
+arr1 = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12])
+newarr = arr1.reshape(4, 3)
+print("Converting 1-D to 2-D:")
+print(newarr)
+
+# converting 1-D to 3-D
+newarr1 = arr1.reshape(2, 3, 2)
+print("Converting 1-D to 3-D:")
+print(newarr1)
+
+# flattening of an array i.e. converting 2-D or 3-D to 1-D
+newarr2 = arr.reshape(-1)
+print("Flattening of an array:")
+print(newarr2)
